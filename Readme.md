182b01917aa48a1a33c1c52744bea7db10e14069
Justin Capalbo <capalbo.justin@gmail.com>
2018-10-07 20:53:28 -0400

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# Blackjack (21) #
+
+### Usage instructions ###
+
+1. Run `npm install` to install dependencies
+2. Use `npm run dev` to run in development mode
+3. Browse to `http://localhost:1234`
---
+++ src/components/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Component } from 'react';
+
+export class App extends Component {
+  render() {
+    return (
+      <div>Who made these brackets?</div>
+    );
+  }
+}
